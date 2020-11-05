4b899e22775eb9bd6e8fee1a8a3ddde8fb25b150
gihoon <zaragee@nate.com>
2020-11-05 23:10:24 +0900

    first commit

---
+++ src/main/java/pizza/external/PaymentService.java
@@ -0,0 +1,18 @@
+
+package pizza.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+//@FeignClient(name="payment", url="http://payment:8080")
+@FeignClient(name="Payment", url="http://localhost:8082")
+public interface PaymentService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/payments")
+    public void doPayment(@RequestBody Payment payment);
+
+}
